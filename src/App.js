0323c855677ebb9608fe271d25900f979ba3f2ec
Natsuki <nkondo3@gmail.com>
2020-10-21 14:58:20 +0900

    fixed top-moving -f

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Navbar from './components/pages/Navbar'
 import { BrowserRouter as Router, Switch , Route} from 'react-router-dom'
@@ -8,11 +8,17 @@ import Profile from './components/pages/Profile/Profile'
 import Services from './components/pages/Services/Services'
 
 function App() {
+
+  // const [click, setClick] = useState();
+  // const handleClick = () => setClick(false);
+
   return (
     <Router>
       <Navbar />
-      {/* <Home /> */}
-      {/* これが元凶だった */}
+      <div className="first-top"  >
+        { Switch ? '' : <Home /> }
+      </div>
+      {/* これが元凶 cf.Navbar*/}
       <Switch>
         <Route path='/' exact component={Home}/>
         <Route path='/Home'  component={Home}/>
